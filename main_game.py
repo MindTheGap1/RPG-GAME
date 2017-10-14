b807eaca418f44c95513584ab3c89458f64a7be2
unwoundclock <unwoundclock@gmail.com>
2017-10-14 01:27:02 +0100

    This is the Main Game File where we will all combine our efforts, basically.

---
+++ main_game.py
@@ -0,0 +1,10 @@
+###MAIN RPG GAME CODE
+
+###My suggestion would be for programmers to individually work on big
+##function defs and then put them in here when you're done like
+##intro() or dungeon_code()
+##to keep this file clean
+
+##Hello non-devs too :)
+
+from Mel_funcs import *
